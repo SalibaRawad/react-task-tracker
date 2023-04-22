73e2e7f418daf5db9d405e1a0f1101171c77ba4a
Rawad Saliba <rawad45saliba@gmail.com>
2023-04-22 16:48:18 +0300

    Second comit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from './components/Header'
 import Tasks from './components/Tasks';
+import AddTask from "./components/AddTask";
 function App() {
   const [tasks, setTask] = useState([
     {
@@ -22,6 +23,14 @@ function App() {
       reminder: false,
     },
   ]);
+
+  //Add Task
+  const addTask = (task) =>{
+    const id = Math.floor(Math.random() * 10000)+1
+    const newTask = {id,...task}
+    setTask([...tasks,newTask])
+  }
+
   //Delete Task
   const deleteTask = (id) =>{
     setTask(tasks.filter((task)=>task.id !== id ))
@@ -29,13 +38,15 @@ function App() {
 
   //Toggle Reminder
   const toggleReminder = (id) =>{
-    console.log("toggle")
+    setTask(tasks.map((task) => task.id === id?
+    {...task,reminder:!task.reminder}:task))
   }
 
   return (
     <div className="container">
       <Header ></Header>
-      { tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask}></Tasks> : "No Tasks to Show"}
+      <AddTask onAdd={addTask}></AddTask>
+      { tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}></Tasks> : "No Tasks to Show"}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,17 @@
 import Task from "./Task";
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   //state is immutable so we cannot write tasks.push but rather use setTask (e.g setTask(...tasks,{add new object here}))
   //if we want to use this state in another component we use context api or redux or make the state global like we did in App.js
 
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={onDelete}></Task>
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        ></Task>
       ))}
     </>
   );
